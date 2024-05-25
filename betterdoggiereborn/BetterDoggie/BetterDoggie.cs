7b61efe24b3c37a1990a5f92c941cda99dff70b1
1EnesBaturKaza <156574288+1EnesBaturKaza@users.noreply.github.com>
2024-05-25 12:47:56 +0300

    Update BetterDoggie.cs

--- betterdoggiereborn/BetterDoggie/BetterDoggie.cs
+++ betterdoggiereborn/BetterDoggie/BetterDoggie.cs
@@ -1,4 +1,4 @@
-﻿namespace BetterDoggie
+namespace BetterDoggie
 {
     using System;
     using System.Collections.Generic;
@@ -15,10 +15,6 @@
         public override string Author => "Parkeymon";
         public override string Name => "BetterDoggie";
         public override string Prefix => "better_doggie";
-        public override Version Version => new Version(1, 3, 1);
-        public override Version RequiredExiledVersion => new Version(5, 0, 0);
-        public override PluginPriority Priority => PluginPriority.Low;
-
         public Dictionary<Player, CoroutineHandle?> ActiveAbilities = new Dictionary<Player, CoroutineHandle?>();
         public Dictionary<Player, int> AbilityCooldowns = new Dictionary<Player, int>();
         
